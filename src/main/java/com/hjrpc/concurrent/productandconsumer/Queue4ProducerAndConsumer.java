21947182b14bc4b8ae78a6361479fd94e7a622a2
qq502361472 <502361472@qq.com>
2019-06-26 17:41:31 +0800

    producer and consumer

---
+++ src/main/java/com/hjrpc/concurrent/productandconsumer/Queue4ProducerAndConsumer.java
@@ -0,0 +1,54 @@
+package com.hjrpc.concurrent.productandconsumer;
+
+import java.util.Random;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class Queue4ProducerAndConsumer {
+    private BlockingQueue<Integer> queue = new LinkedBlockingQueue<>();
+    public Producer producer = new Producer();
+    public Consumer consumer = new Consumer();
+    public Random random = new Random();
+
+    public Queue4ProducerAndConsumer(BlockingQueue<Integer> queue){
+        this.queue = queue;
+    }
+
+    class Producer implements Runnable{
+        @Override
+        public void run() {
+            for (;;){
+                try {
+                    int i = random.nextInt(100);
+                    queue.put(i);
+                    System.out.println("producer produce num:"+i);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    Thread.sleep(random.nextInt(1000));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    class Consumer implements Runnable{
+        @Override
+        public void run() {
+            try {
+                Integer take = queue.take();
+                System.out.println("consumer consume num:"+take);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                Thread.sleep(random.nextInt(1000));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
